2cc4575cd5692846fb7ca87c44a89d13b2309b5c
Joey V <joey03v@gmail.com>
2024-02-09 19:19:19 -0600

    finished userroutes

---
+++ routes/api/userroute.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { getAllUsers, getUserById, createUser,updateUser,addFriend,removeFriend, deleteUser
+} = require("../controllers/usercontrol")
+
+
+router.route("/").get(getAllUsers).post(createUser)
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser)
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend)
+
+module.exports = router
